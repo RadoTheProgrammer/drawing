30a37399d5412fff8616a21c5de99b5de45b0605
RadoTheProgrammer <rado@arazakar.com>
2024-04-06 16:13:51 +0200

    removed chess cuz it's broken, added new project drawing

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 # fill the screen with a color to wipe away anything from last frame
-screen.fill("purple")
+# create a surface with the same size as the screen
+drawing = pygame.Surface(screen.get_size())
+# fill the surface with purple color
+drawing.fill("purple")
+# blit the drawing surface onto the screen
+
 mousedown=False
+mousepos=(0,0)
 while running:
 
     
@@ -20,6 +26,7 @@ while running:
     # pygame.QUIT event means the user clicked X to close your window
     #mouse_pos = pygame.mouse.get_pos()
     #print(mouse_pos)
+    screen.blit(drawing, (0, 0))
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:
@@ -29,12 +36,16 @@ while running:
         elif event.type == pygame.MOUSEBUTTONUP:
             mousedown=False
         elif event.type == pygame.MOUSEMOTION:
-            if mousedown:
-                pygame.draw.ellipse(screen, "red", (*event.pos, 50, 50))
+            mousepos=event.pos
+    if mousedown:
+        pygame.draw.ellipse(drawing, "red", (*mousepos, 50, 50))
+    else:
+        pygame.draw.ellipse(screen, "red", (*mousepos, 50, 50))
             #mouse_pos = pygame.mouse.get_pos()
             #pygame.draw.ellipse(screen, "red", (*mouse_pos, 50, 50))
             #pygame.draw.ellipse(screen, "red", (*event.pos,50, 50))
     # flip() the display to put your work on screen
+    
     pygame.display.flip()
 
     clock.tick(60)  # limits FPS to 60
