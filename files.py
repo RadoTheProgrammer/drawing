8bb8ef0fff4f13949d74d9f5410f1eb86a54b81a
RadoTheProgrammer <rado@arazakar.com>
2024-04-07 15:07:53 +0200

    started files.py

--- files.py
+++ files.py
@@ -1,9 +1,19 @@
 import os
+
+from pyparsing import C
 DIR="drawings"
+CURRENT_FILE=os.path.join(DIR,"current.png")
+
+def switch():
+    global filename
+    filename=os.path.join(DIR,filename_tag)
+    os.remove(CURRENT_FILE)
+    os.symlink(filename_tag,CURRENT_FILE)
 
 if os.path.exists(DIR):
-    filename_tag=os.readlink(os.path.join(DIR,"current.png"))
+    filename_tag=os.readlink(os.path.join(DIR,CURRENT_FILE))
     filename=os.path.join(DIR,filename_tag)
 else:
     os.mkdir(DIR)
     filename_tag="drawing_0.png"
+    
